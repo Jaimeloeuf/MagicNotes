dd6f47e651e5c9c06b1b930bc907660359205a83
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-04-25 21:18:09 +0800

    AllNotesComponent: 1.  Added in some scaffold code for the HTML file
    2.  Wrote the AllNotesComponent class.

--- MagicNotes/lib/src/AllNotes/all_notes_component.dart
+++ MagicNotes/lib/src/AllNotes/all_notes_component.dart
@@ -1,10 +1,45 @@
+import 'dart:async';
+import 'package:MagicNotes/src/Note.dart';
+import 'package:MagicNotes/src/NotesService.dart';
+import 'package:MagicNotes/src/route_paths.dart';
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+
+// import the notes service
+
 
 @Component(
   selector: 'all-notes',
   templateUrl: 'all_notes_component.html',
   styleUrls: ['all_notes_component.css'],
-  directives: [NgFor, NgIf],
-  providers: [],
-)
-class SearchComponent {}
+  directives: [NgFor, NgIf])
+class AllNotesComponent implements OnInit {
+  Note selected_note;
+  List<Note> _notes;
+
+  get notes => _notes;
+
+  // Define hero service property for this component
+  final NotesService _notesService;
+  final Router _router;
+
+  // AppComponent Contructor that takes in a HeroService instance, by dependency injection.
+  // Inject the router too
+  AllNotesComponent(this._notesService, this._router);
+
+  // On click handler, that takes in the selected hero
+  void onSelect(Note hero) => selected_note = hero;
+
+  // Set the list of heroes from the Hero services
+  Future<void> _getNotes() async => _notes = await _notesService.getAll();
+
+  // Implementation of the Component Init lifecycle hook
+  @override
+  void ngOnInit() => _getNotes();
+
+  String noteUrl(int id) =>
+      RoutePaths.notes.toUrl(parameters: {RoutePaths.id: '$id'});
+
+  Future<NavigationResult> viewNote() =>
+      _router.navigate(noteUrl(selected_note.id));
+}
