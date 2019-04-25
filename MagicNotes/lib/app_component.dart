5b0093b683d139a0589066135fdc02f320025bb7
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-04-25 18:51:50 +0800

    Changes: 1.  Updated the web\index.html file parts to use MagicNote words.
    2.  Created a new HeaderComponent and its corresponding directory in src
        directory and implemented the Component into the app_component files.

--- MagicNotes/lib/app_component.dart
+++ MagicNotes/lib/app_component.dart
@@ -1,12 +1,13 @@
 import 'package:angular/angular.dart';
 
 import 'src/todo_list/todo_list_component.dart';
+import 'package:MagicNotes/src/HeaderComponent/header_component.dart';
 
 @Component(
-  selector: 'my-app',
+  selector: 'MagicNote',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [TodoListComponent, HeaderComponent],
 )
 class AppComponent {
 }
---
+++ MagicNotes/lib/src/HeaderComponent/header_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'header',
+  templateUrl: 'header_component.html',
+  styleUrls: ['header_component.css'],
+  
+)
+class HeaderComponent {
+  
+}
