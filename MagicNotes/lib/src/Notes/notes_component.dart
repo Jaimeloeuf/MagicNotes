4a0f8be8d1ff0afee5a4e4616f38f3746589b1e9
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-05-03 16:19:40 +0800

    Update to notes_component: 1.  Updating the component to use the new NotesBloc following the BLoC pattern     instead of the previous NotesService object dependency. Removed NotesService     and added NotesBloc dependency into the module.
    2.  Updated the constructor to take the dependency.
    
    3.  Working on the component now to deal with the creation and destruction
        of the notes editor component, in the same display screen.

--- MagicNotes/lib/src/Notes/notes_component.dart
+++ MagicNotes/lib/src/Notes/notes_component.dart
@@ -1,17 +1,21 @@
 import 'dart:async';
+import 'package:MagicNotes/blocs/notes_bloc.dart';
 import 'package:MagicNotes/src/Note.dart';
-import 'package:MagicNotes/src/notes_service.dart';
 import 'package:MagicNotes/src/route_paths.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
 /*
   @Doc
-  This module holds the component that displays all the Notes.
+  This component that displays the notes, that matches the view schema.
+  The view schema is provided by either the view by tag service or the view by search service
 
   @Todo
-  - Move the dependency injection of the NotesService out of the @Component annotation
-    and into the main dependency injector creator and injection point in main.dart
+  - Move the dependency injection of the NotesService out of the @Component annotation and into the main dependency injector creator and injection point in main.dart
+
+  - Perhaps the dependency injected should be changed to notesbloc and injected from the main injection point.
+
+  - Updating so that this component is not destroyed everytime user navigates away and recreated when the user navigates to the route of notes again. Which is problematic due to the need to recreate component redraw and requiring the same resources from the server again and again.
 */
 
 @Component(
@@ -19,51 +23,44 @@ import 'package:angular_router/angular_router.dart';
     templateUrl: 'notes_component.html',
     styleUrls: ['notes_component.css'],
     directives: [NgFor, NgIf],
-    /* providers: [ClassProvider(NotesService)] */)
-class AllNotesComponent implements OnInit, OnActivate {
-  Note selected_note;
+    providers: [/* ClassProvider(NotesService) */])
+class AllNotesComponent implements OnInit /* OnActivate  */ {
   List<Note> _notes;
 
-  final NotesService _notesService;
+  // final NotesService _notesService;
+  final NotesBloc _notesBloc;
   final Router _router;
 
   // AppComponent Contructor takes in _notesService and router dependency via injection.
-  AllNotesComponent(this._notesService, this._router);
+  AllNotesComponent(this._notesBloc, this._router);
 
+  // Getter for the template to access the private _notes field
+  // To change this to attach the List of notes from a stream from the NotesService
   get notes => _notes;
-
-  // On click handler that takes in the selected note
-  void onSelect(Note note) {
-    // Set the note to be the selected_note first
-    selected_note = note;
-
-    // Navigate to the /note/:id route for the user to edit that note directly.
-    viewNote();
-  }
 
   // Set the list of notes from the _notesService
   // Update this, to make the notes component stream data from the service, and only get data when data is changed.
-  Future<void> _getNotes() async {
-    _notes = await _notesService.getAll();
-    print('Below is the received list');
-    for (int i = 0; i < notes.length; i++) print(notes[i].content);
-  }
+  Future<void> _getNotes() async => _notes = await _notesBloc.getAll();
 
   // Implementation of the Component Init lifecycle hook
   @override
   void ngOnInit() => _getNotes();
 
-  @override
+  // Might remove the ngOnInit hook, as the component should not be destroyed and recreated, but rather when navigated too
+  // And it should not be updating the whole thing, it should only check if there is anything changed, else do nothing.
+  // See how to make the _notes be available from a stream instead, thus it can always stream data to and from the notesService
+  // and the notesComponent no longer needs to maintain a copy of the _notes in memory!
+  /*  @override
   void onActivate(_, RouterState current) {
     print('Notes route activated');
     _getNotes();
-  }
+  } */
 
   // Given a note id, create a URL for that note and return it.
   String noteUrl(int id) =>
       RoutePaths.note.toUrl(parameters: {RoutePaths.idParam: '$id'});
 
-  // Navigate to the note editor displaying the selected note
-  Future<NavigationResult> viewNote() =>
+  // onClick handler that navigates to selected note's "/note/:id" route for user to edit the note
+  Future<NavigationResult> viewNote(Note selected_note) =>
       _router.navigate(noteUrl(selected_note.id));
 }
